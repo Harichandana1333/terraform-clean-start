c4f3127414164d903d80b88d6ff6efd4545cdc7b
Harichandana1333 <harichandana1333@gmail.com>
2025-04-03 22:31:53 -0700

    Remove GCS backend to fix billing issue

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-        terraform {
-         backend "gcs" {
-             bucket = "hari_bucket_6"
-             prefix = "terraform/state"
+  #      terraform {
+   #      backend "gcs" {
+    #         bucket = "hari_bucket_6"
+     #        prefix = "terraform/state"
    
-           }
-         }
+      #     }
+       #  }
 
